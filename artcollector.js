6a99fe426dcb77aed6f4db372f1d7f954c19179b
Patricio Calderon <ourobor@gmail.com>
2016-03-12 14:54:36 -0500

    adds a table to lay out names of artists and adds a time using setInterval

--- artcollector.js
+++ artcollector.js
@@ -1,23 +1,57 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if(Meteor.isClient) {
+  Template.artists.helpers({
+    'artist': function() {
+      return MyArtists.find({}, {sort: {name: 1}});
+    },
+    'selectedClass': function() {
+      var artistId = this._id;
+      var selectedArtist = Session.get('selectedArtist');
+      if(artistId === selectedArtist) {
+        return "selected";
+      }
+    },
+    'weightOfFont': function() {
+        var artistId = this._id;
+        var overArtist = Session.get('overArtist');
+        if(artistId === overArtist) {
+          return "emphasis";
+        }
+      }
+
+  });
+
+
+  // This function allows the date to change every second.
+  Meteor,setInterval(function() {
+    Session.set("current_date", new Date());
+  }, 1000);
+
+  // This helper function gets the Session information from the setInterval
+  Template.showDate.helpers({
+    'actual_date': function() {
+      return Session.get("current_date");
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+
+
+
+  Template.artists.events({
+    'click .uniqueArtist': function() {
+      var artistId = this._id;
+      Session.set('selectedArtist', artistId);
+      var selectedArtist = Session.get('selectedArtist');
+      console.log(selectedArtist);
+    },
+    'mouseover .uniqueArtist': function() {
+      var artistId = this._id;
+      Session.set('overArtist', artistId);
+      var overArtist = Session.get('overArtist');
     }
+
   });
+
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+MyArtists =  new Mongo.Collection('artists');
